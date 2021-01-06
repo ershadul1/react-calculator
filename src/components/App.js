0dbf17606c3f3b6ea3954d4b8846fdb7c228a1e2
ershadul1 <ershadul.rayhan@gmail.com>
2021-01-06 23:38:39 +0600

    Create app components folder

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Display from './Display'
+import ButtonPanel from './ButtonPanel'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello! React</h1>
+      <Display />
+      <ButtonPanel />
+    </div>
+    );
+}
+
+export default App;

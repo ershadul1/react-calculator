cac0ffb576cbed2905a39e6f6d1d77fffbb97355
ershadul1 <ershadul.rayhan@gmail.com>
2021-01-08 18:00:27 +0600

    Convert App  into class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-const App = () => (
-  <>
-    <h1>Calculator</h1>
-    <Display result="0" />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <>
+        <h1>Calculator</h1>
+        <Display result={total || '0'} next={next} operation={operation} />
+        <ButtonPanel />
+      </>
+    );
+  }
+}
 
 export default App;

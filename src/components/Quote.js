e3f378bced2b18f027d2b8cd8606d159912592ea
ershadul1 <ershadul.rayhan@gmail.com>
2021-01-14 22:52:21 +0600

    Add quote page

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Quote = () => (
+  <div id="quote">
+    <h1>Quote</h1>
+    <p>
+      “The difference between the poet and the mathematician is that
+      the poet tries to get his head into the heavens while the mathematician
+      tries to get the heavens into his head.”
+      ― G.K. Chesterton
+    </p>
+  </div>
+);
+
+export default Quote;
